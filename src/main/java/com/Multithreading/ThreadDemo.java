e9cc23959e478985c8ebbaa096aa5325417aceb9
raj <raj@gmail.com>
2022-12-19 12:20:34 +0530

    Thread with runnalbe

--- src/main/java/com/Multithreading/ThreadDemo.java
+++ src/main/java/com/Multithreading/ThreadDemo.java
@@ -8,10 +8,22 @@ public class ThreadDemo extends Thread{
         System.out.println("hello");}
     }
 }
-
- class Threaddeomo1{
+class thread1 implements Runnable{
+    @Override
+    public void run() {
+        for (int i=0;i<1400;i++){
+            System.out.println("hello ravi how are yuo");
+        }
+    }
+}
+class Threaddeomo1 {
      public static void main(String[] args) {
          ThreadDemo threadDemo =new ThreadDemo();
+         thread1 t =new thread1();
+         Thread t1 =new Thread(t);
+         t1.start();
+
+         System.out.println("hii");
          threadDemo.start();
 
          for (int i=0;i<139999;i++){
